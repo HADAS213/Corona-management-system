fd6e0a1d297a577b415bf504d8370cf365b7c2c4
HADAS213 <116496491+HADAS213@users.noreply.github.com>
2022-10-23 18:38:14 +0300

    add client and server project

---
+++ hospital-dashboard/src/model/Member.ts
@@ -0,0 +1,22 @@
+interface Address{
+  city: string;
+  street: string;
+  numHouse:number;
+}
+interface Vaccination{
+ vacDate:Date ;
+ manufacturer:string;
+}
+
+export interface Member {
+  id: string;
+  firstName:string;
+  lastName:string;
+  address:Address;
+  birthDate:Date;
+  phone:string;
+  cellPhone:string;
+  positiveResultDate:Date;
+  recoveryDate:Date;
+  vaccinations:Vaccination[];
+}
